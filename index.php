db7213d522b86c5fbe9dd214aa6cc42bb533f8ee
Metha <you@example.com>
2024-11-25 12:38:58 +0700

    reset

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+
+<link rel="stylesheet" href="bootstrap-5.2.1-dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="jquery-3.6.1.min.js">
+<script src="bootstrap-5.2.1-dist/js/bootstrap.bundle.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+<title>adf</title>
+</head>
+
+<div class="row">
+<!----logo-----แถวที่1--->
+  <div class="col-4 bg-danger">55555</div>
+    <div class="col-4 bg-success">56454</div>
+    <div class="col-4 bg-info">33</div>        
+</div>
+
+
+<div class="row">
+<!----menu----แถวที่2---->
+    <div class="col-4 bg-danger">55555</div>
+    <div class="col-4 bg-success">56454</div>
+    <div class="col-4 bg-info">33</div>
+        
+</div>
+
+
+<div class="row">
+<!----img-slide-----แถวที่3--->
+ <div class="col-1 bg-light"></div>
+ 
+ 
+ <div class="col-10 bg-danger">
+    <div id="demo" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner" align="center">
+	<div class="carousel-item active">
+    	<img src="img/1.png" alt="Com" class="d-block" style="width:10%"></div>
+    <div class="carousel-item">
+    	<img src="img/2.png" alt="Lionfish" class="d-block" style="width:10%"></div>
+ </div>
+  
+ 
+<div class="col-1 bg-light"></div>
+
+
+  <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon">
+    </span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+    <span class="carousel-control-next-icon">
+    </span>
+  </button>
+
+
+</div>
+</div>
+</div>
+<body>
+</body>
+</html>
